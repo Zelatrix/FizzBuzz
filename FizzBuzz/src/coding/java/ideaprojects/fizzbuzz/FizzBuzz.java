fdc2696d0c8c99167794bf855dda596e54c820cc
Zelatrix <39635013+Zelatrix@users.noreply.github.com>
2018-12-19 18:55:50 +0000

    Add files via upload

---
+++ FizzBuzz/src/coding/java/ideaprojects/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,51 @@
+package coding.java.ideaprojects.fizzbuzz;
+
+/**
+ * Write a description of class FizzBuzz here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FizzBuzz
+{
+    private int i;
+    /**
+     * Constructor for objects of class FizzBuzz
+     */
+    public FizzBuzz()
+    {
+        // initialise instance variables
+        i = 0;
+    }
+
+    /**
+     * If x is a multiple of 3, print "fizz"
+     * If x is a multiple of 5, print "buzz"
+     * If x is a multiple of both 3 and 5, print "fizzbuzz"
+     */
+    public void FizzBuzz()
+    {
+        for(i=1; i<=10; i++)
+        {
+            if(i%3==0 && i%5==0)
+            {
+                System.out.println("FizzBuzz");
+            }
+
+            else if(i%3==0)
+            {
+                System.out.println("Fizz");
+            }
+
+            else if(i%5==0)
+            {
+                System.out.println("Buzz");
+            }
+
+            else
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ FizzBuzz/src/coding/java/ideaprojects/fizzbuzz/Main.java
@@ -0,0 +1,9 @@
+package coding.java.ideaprojects.fizzbuzz;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        FizzBuzz f = new FizzBuzz();
+    }
+}
