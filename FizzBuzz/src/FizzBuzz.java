74f5a372173e912e1f9da9d481e4e635bf646047
Callum Stewart <4onthefly@gmail.com>
2021-01-30 21:03:49 +0000

    Added an improved version of the function

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -4,49 +4,29 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class FizzBuzz
-{
-    public void main(String[] args)
-    {
-        FizzBuzz f = new FizzBuzz();
-    }
-
-    /**
-     * Constructor for objects of class FizzBuzz
-     */
-    public FizzBuzz()
-    {
-        // initialise instance variables
-    }
+public class FizzBuzz {
+    public void main(String[] args) { FizzBuzz f = new FizzBuzz(); }
 
     /**
      * If x is a multiple of 3, print "fizz"
      * If x is a multiple of 5, print "buzz"
      * If x is a multiple of both 3 and 5, print "fizzbuzz"
      */
-    public void FizzBuzz()
-    {
-        for(int i=1; i<=100; i++)
-        {
-            if(i%3==0 && i%5==0)
-            {
-                System.out.println("FizzBuzz");
-            }
+    public void FizzBuzz() {
+        for(int i=1; i<=100; i++) {
+            if(i%3==0 && i%5==0) { System.out.println("FizzBuzz"); }
+            else if(i%3==0) { System.out.println("Fizz"); }
+            else if(i%5==0) { System.out.println("Buzz"); }
+            else { System.out.println(i); }
+        }
+    }
 
-            else if(i%3==0)
-            {
-                System.out.println("Fizz");
-            }
-
-            else if(i%5==0)
-            {
-                System.out.println("Buzz");
-            }
-
-            else
-            {
-                System.out.println(i);
-            }
+    public void FBImproved() {
+        switch(num) {
+            case (num % 3 == 0 && num % 5 == 0) { System.out::print("FizzBuzz") }
+            case (num % 3 == 0) { System.out::print("Fizz") }
+            case (num % 5 == 0) { System.out::print("Buzz") }
+            default { System.out::print(i); }
         }
     }
 }
